e81f59d4c517f925c37e24184127d45eb700a4a3
Abhishek Patel <abhipatel@pop-os.localdomain>
2020-12-29 12:39:23 +0530

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '905827575230-9pu7ptphh0pi60jnj6lu8qnb8uojcrp1.apps.googleusercontent.com',
+  googleClientSecret: 's15GejUQDsiCSY3Dgq5JtMp7',
+  mongoURI:
+    'mongodb+srv://admin:admin123@emaily.ouy4t.mongodb.net/emaily?retryWrites=true&w=majority',
+  cookieKey: 'afnajdbfaknfclnjdkjnaglnjflawnfjdvj',
+};
